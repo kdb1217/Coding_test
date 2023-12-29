55742ac2cb5fd8c908373cc03ad1850716b0ea3d
김다빈 <84852135+kdb1217@users.noreply.github.com>
2023-12-29 15:28:27 +0900

    [Bronze V] Title: 시험 성적, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.swift
@@ -0,0 +1,17 @@
+let score = Int(readLine()!)!
+
+if score >= 90 && score <= 100 {
+    print("A")
+}
+else if score >= 80 && score <= 89 {
+    print("B")
+}
+else if score >= 70 && score <= 79 {
+    print("C")
+}
+else if score >= 60 && score <= 69 {
+    print("D")
+}
+else {
+    print("F")
+}
