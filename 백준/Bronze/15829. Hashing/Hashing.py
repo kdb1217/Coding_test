b84fc855bf8ed2b6b5d21fca1d81682a2269d7f0
김다빈 <84852135+kdb1217@users.noreply.github.com>
2024-03-02 18:02:26 +0900

    [Bronze II] Title: Hashing, Time: 128 ms, Memory: 108080 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -2,7 +2,8 @@ inputNum = int(input())
 inputString = input()
 answer = 0
 for i in range(inputNum):
-    answer += (ord(inputString[i]) - 96) * (31 ** i)
+    answer += ((ord(inputString[i]) - 96) * (31 ** i)) 
+    answer = answer % 1234567891
 
 
 print(answer)
