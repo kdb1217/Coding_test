0a44870b985cc2941caf1a05f21f0c6692c2fcc9
김다빈 <84852135+kdb1217@users.noreply.github.com>
2024-03-22 16:43:34 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -1,12 +1,13 @@
 def solution(s):
-    s = list(s)
-    if s[0].isalpha():
-        s[0] = s[0].upper()
-    for i in range(1,len(s)):
-        if s[i - 1].isalpha() or s[i - 1].isdigit():
-            s[i] = s[i].lower()
-        else:
-            s[i] = s[i].upper()
-    return "".join(s)
+    s = list(s.split())
+    print(s)
+    for i in range(len(s)):
+        s[i] = checkString(s[i])
+    return " ".join(s)
 
+def checkString(s):
+    if s[0].isalpha():
+        return s[0].upper() + s[1:].lower()
+    else:
+        return s[0] + s[1:].lower()
 
