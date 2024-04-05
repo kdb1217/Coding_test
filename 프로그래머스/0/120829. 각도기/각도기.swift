9a286e3099235a5839d82cdfa383714da879f4cf
김다빈 <84852135+kdb1217@users.noreply.github.com>
2024-04-05 17:05:20 +0900

    [level 0] Title: 각도기, Time: 0.01 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+func solution(_ angle:Int) -> Int {
+    if angle == 90 {
+        return 2
+    }
+    else if angle > 0 && angle < 90 {
+        return 1
+    }
+    else if angle > 90 && angle < 180 {
+        return 3
+    }
+    else {
+        return 4
+    }
+}
