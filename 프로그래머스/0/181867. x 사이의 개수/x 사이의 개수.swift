5f61a12d2612a7ed74a33a5a916b69dd3d5bfe50
김다빈 <84852135+kdb1217@users.noreply.github.com>
2024-04-18 18:23:04 +0900

    [level 0] Title: x 사이의 개수, Time: 29.78 ms, Memory: 17.7 MB -BaekjoonHub

--- 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.swift
+++ 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.swift
@@ -1,17 +1,5 @@
 import Foundation
 
 func solution(_ myString:String) -> [Int] {
-    var cnt: Int = 0
-    var answer: [Int] = []
-    for i in myString {
-        if i == "x" {
-            answer.append(cnt)
-            cnt = 0
-        } else {
-            cnt += 1
-        }
-    }
-    answer.append(cnt)
-
-    return answer
+    return myString.components(separatedBy: "x").map { $0.count }
 }
