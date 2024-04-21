a29de7b56061bae5f23d26803bfa35906e170b91
김다빈 <84852135+kdb1217@users.noreply.github.com>
2024-04-21 21:50:07 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.02 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.swift
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.swift
@@ -1,27 +1,5 @@
 import Foundation
 
 func solution(_ keyinput:[String], _ board:[Int]) -> [Int] {
-    var answer = [0,0]
-    for i in keyinput {
-        switch i {
-            case "up":
-                if board[1] / 2 > answer[1] {
-                    answer[1] += 1
-                }
-            case "down":
-                if -(board[1] / 2) < answer[1] {
-                    answer[1] -= 1
-                }
-            case "left":
-                if -(board[0] / 2) < answer[0] {
-                    answer[0] -= 1
-                }
-            default:
-                if board[0] / 2 > answer[0] {
-                    answer[0] += 1
-                }
-                
-        }
-    }
-    return answer
+    return []
 }
