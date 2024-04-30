38da6d4af9bae348e4c17b58132d0ebc77598b28
김다빈 <84852135+kdb1217@users.noreply.github.com>
2024-05-01 01:09:46 +0900

    [Bronze II] Title: 벌집, Time: 12 ms, Memory: 79512 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.swift
@@ -0,0 +1,80 @@
+import Foundation
+
+// 백준 전용 입출력
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+
+var n = Int(readLine()!)!
+
+/*
+1
+2 ~ 7 [2,3,4,5,6,7] 6
+8 ~ 19 [8,9,10,11,12,13,14,15,16,17,18,19] 12
+20 ~37 18
+38 ~61 24
+*/
+
+var cnt: Int = 1
+var tmp: Int = 1
+
+while tmp < n {
+    tmp += 6 * cnt
+    cnt += 1
+}
+print(cnt)
+
