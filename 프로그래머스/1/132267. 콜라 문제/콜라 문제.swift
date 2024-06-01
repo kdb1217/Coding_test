a5719a7b03fea36e110194407f326aabff98465e
김다빈 <84852135+kdb1217@users.noreply.github.com>
2024-06-01 13:24:35 +0900

    [level 1] Title: 콜라 문제, Time: 0.01 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.swift
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.swift
@@ -1,18 +1,16 @@
 import Foundation
-// a 빈병을 a개를 주면 b개의 콜라를 준다.
-// b 빈병 a개를 줬을때 다시 받는 콜라의 수
+
 func solution(_ a:Int, _ b:Int, _ n:Int) -> Int {
-    var CokeCount: Int = 0
-    var emptiedBottle: Int = n
+    var stock: Int = n
+    var k = n
+    var answer: Int = 0
+    var tmp: Int = 0
     
-    while true {
-        if emptiedBottle >= a {
-            emptiedBottle = emptiedBottle - a
-            emptiedBottle += b
-            CokeCount += b
-        } else {
-            break
-        }
-    }
-    return CokeCount
+    while (k > a) {
+        tmp += (k / a) * b
+        stock = k % a
+        answer += tmp
+        k = tmp + stock
+    } 
+    return answer
 }
