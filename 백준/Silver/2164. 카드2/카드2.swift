77ea7ba36750dbd2062a923e5ba7684a8ed82561
김다빈 <84852135+kdb1217@users.noreply.github.com>
2024-06-11 14:59:05 +0900

    [Silver IV] Title: 카드2, Time: 36 ms, Memory: 91996 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.swift
@@ -0,0 +1,107 @@
+import Foundation
+
+//final class FileIO {
+//    private let buffer:[UInt8]
+//    private var index: Int = 0
+//
+//    init(fileHandle: FileHandle = FileHandle.standardInput) {
+//        
+//        
+//        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+//    }
+//
+//    @inline(__always) private func read() -> UInt8 {
+//        defer { index += 1 }
+//
+//        return buffer[index]
+//    }
+//
+//    @inline(__always) func readInt() -> Int {
+//        var sum = 0
+//        var now = read()
+//        var isPositive = true
+//
+//        while now == 10
+//                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+//        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+//        while now >= 48, now <= 57 {
+//            sum = sum * 10 + Int(now-48)
+//            now = read()
+//        }
+//
+//        return sum * (isPositive ? 1:-1)
+//    }
+//
+//    @inline(__always) func readString() -> String {
+//        var now = read()
+//
+//        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+//
+//        let beginIndex = index-1
+//
+//        while now != 10,
+//              now != 32,
+//              now != 0 { now = read() }
+//
+//        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+//    }
+//
+//    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+//        var now = read()
+//
+//        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+//
+//        let beginIndex = index-1
+//
+//        while now != 10,
+//              now != 32,
+//              now != 0 { now = read() }
+//
+//        return Array(buffer[beginIndex..<(index-1)])
+//    }
+//}
+
+struct Queue {
+    private var arr: [Int] = []
+    private var index: Int = 0
+    
+    var size: Int {
+        return arr.count - index
+    }
+    
+    var isEmpty: Bool {
+        return arr.isEmpty
+    }
+    
+    var last: Int {
+        return arr.last ?? -1
+    }
+    
+    mutating func push(_ n: Int ) {
+        arr.append(n)
+    }
+    
+    @discardableResult
+    mutating func pop() -> Int {
+        if arr.isEmpty {
+            return -1
+        } else {
+            let tmp: Int = arr[index]
+            index += 1
+            return tmp
+        }
+    }
+}
+let n = Int(readLine()!)!
+var queue = Queue()
+
+for i in 1...n {
+    queue.push(i)
+}
+
+while queue.size != 1 {
+    queue.pop()
+    queue.push(queue.pop())
+}
+
+print(queue.last)
