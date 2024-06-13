ed89c74440df16689a89c86351c66a1d110e79b9
김다빈 <84852135+kdb1217@users.noreply.github.com>
2024-06-13 14:28:21 +0900

    [level 0] Title: 다항식 더하기, Time: 0.08 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.swift
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.swift
@@ -3,33 +3,11 @@ import Foundation
 func solution(_ polynomial:String) -> String {
     var xCounter: Int = 0
     var numCounter: Int = 0
-    var answer = ""
     var arr = polynomial.split(separator: " ").map{String($0)}
-    for i in arr {
-        if i.last! == "x" {
-            if i.count == 1 {
-                xCounter += 1
-            } else {
-                var tmp = i.map { String($0) }
-                tmp.removeLast()
-                let num = Int(tmp.joined())!
-                xCounter += num
-            }
-        }
-        if i.allSatisfy({ $0.isNumber }) {
-            numCounter += Int(i)!
-        }
-    }
-    if xCounter == 1 {
-        answer += "x"
-    } else if xCounter > 1 {
-        answer += String(xCounter)+"x"
-    }
-    
-    if numCounter > 0 && xCounter > 0 {
-        answer += " + \(numCounter)"
-    } else if numCounter > 0 {
-        answer += "\(numCounter)"
-    }
-    return answer
+    var numArr = arr.filter { $0.isNumber() }
+    var xArr = arr.filter { $0.contains("x") }
+    print(numArr)
+    print(xArr)
+
+    return ""
 }
