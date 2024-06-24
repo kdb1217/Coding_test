f60f16225c73adec4d696e17a78d8edfe28261b5
김다빈 <84852135+kdb1217@users.noreply.github.com>
2024-06-24 20:43:12 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.65 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.swift
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.swift
@@ -1,24 +1,5 @@
 import Foundation
 
-/*
-풀이계획
-1. 각각의 유형마다 딕셔너리로 초기화 한다.
-*/
 func solution(_ survey:[String], _ choices:[Int]) -> String {
-    var result: [String : Int] = ["R": 0,"T": 0,"C": 0, "F" : 0, "J": 0, "M": 0, "A": 0, "N": 0]
-    var answer = ""
-    for i in 0..<survey.count {
-        let postive = String(survey[i].first!)
-        let negative = String(survey[i].last!)
-        if choices[i] > 4 {
-            result[negative, default: 0] += choices[i] % 4
-        } else if choices[i] < 4 {
-            result[postive, default: 0] += abs(choices[i] - 4)
-        }
-    }
-    answer += result["R"]! >= result["T"]! ? "R" : "T"
-    answer += result["C"]! >= result["F"]! ? "C" : "F"
-    answer += result["J"]! >= result["M"]! ? "J" : "M"
-    answer += result["A"]! >= result["N"]! ? "A" : "N"
-    return answer
+    return ""
 }
