4d8b56bd00023deb15063dfb9bf5f4514c739be8
김다빈 <84852135+kdb1217@users.noreply.github.com>
2024-06-27 16:21:46 +0900

    [level 2] Title: 롤케이크 자르기, Time: 869.04 ms, Memory: 60.7 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.swift
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.swift
@@ -2,25 +2,11 @@ import Foundation
 
 func solution(_ topping:[Int]) -> Int {
     var count = 0
-    var brotherDict: [Int:Int] = [:]
-    var myDict: [Int: Int] = [:]
-    for i in 0..<topping.count {
-        brotherDict[topping[i], default: 0] += 1
-    }
     
-    for i in 0..<topping.count {
-        brotherDict[topping[i]]! -= 1
-        
-        if brotherDict[topping[i]]! == 0 {
-            brotherDict[topping[i]] = nil
-        }
-        
-        myDict[topping[i], default: 0] += 1
-        
-        if brotherDict.keys.count == myDict.keys.count {
+    for i in 0..<topping.count - 1 {
+        if Set(Array(topping[0...i])).count == Set(Array(topping[(i+1)...])).count {
             count += 1
-        }
+        } 
     }
-    
     return count
 }
