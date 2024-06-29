f7cb228824963c12a2531ed563426980862bf9f3
김다빈 <84852135+kdb1217@users.noreply.github.com>
2024-06-29 12:59:47 +0900

    [Silver II] Title: 랜선 자르기, Time: 20 ms, Memory: 79648 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.swift
@@ -0,0 +1,97 @@
+import Foundation
+
+//final class FileIO {
+//    private let buffer:[UInt8]
+//    private var index: Int = 0
+//
+//    init(fileHandle: FileHandle = FileHandle.standardInput) {
+//        
+//        
+//        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+//    }
+//
+//    @inline(__always) private func read() -> UInt8 {
+//        defer { index += 1 }
+//
+//        return buffer[index]
+//    }
+//
+//    @inline(__always) func readInt() -> Int {
+//        var sum = 0
+//        var now = read()
+//        var isPositive = true
+//
+//        while now == 10
+//                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+//        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+//        while now >= 48, now <= 57 {
+//            sum = sum * 10 + Int(now-48)
+//            now = read()
+//        }
+//
+//        return sum * (isPositive ? 1:-1)
+//    }
+//
+//    @inline(__always) func readString() -> String {
+//        var now = read()
+//
+//        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+//
+//        let beginIndex = index-1
+//
+//        while now != 10,
+//              now != 32,
+//              now != 0 { now = read() }
+//
+//        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+//    }
+//
+//    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+//        var now = read()
+//
+//        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+//
+//        let beginIndex = index-1
+//
+//        while now != 10,
+//              now != 32,
+//              now != 0 { now = read() }
+//
+//        return Array(buffer[beginIndex..<(index-1)])
+//    }
+//}
+/*
+ 풀이계획
+ 1. 완탐으로 계산 -> 실패
+ 2. 이분탐색 그러나 How?
+ */
+let arr = readLine()!.split(separator: " ").map { Int(String($0))! }
+
+var k = arr[0]
+var n = arr[1]
+var cnt = 0
+var lens: [Int] = []
+
+for _ in 0..<k {
+    lens.append(Int(readLine()!)!)
+}
+
+var first = 1
+var last = lens.max()!
+var result = 0
+
+while first <= last {
+    let middle = (first + last) / 2
+    let makeLen = lens.map { $0 / middle }.reduce(0, +)
+    
+    if makeLen >= n {
+        if result < middle {
+            result = middle
+        }
+        first = middle + 1
+    } else {
+        last = middle - 1
+    }
+}
+
+print(result)
