b677554bd6e7f6c24ef9d925faf0a43457823ba0
김다빈 <84852135+kdb1217@users.noreply.github.com>
2024-07-03 00:55:01 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 180.74 ms, Memory: 27.4 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.swift
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.swift
@@ -3,13 +3,11 @@ import Foundation
 func solution(_ elements:[Int]) -> Int {
     var numsSet: Set<Int> = []
     let numbers = elements + elements 
-    var tmp = 0
+    
  
     for i in 0..<elements.count {
-        tmp = 0
         for j in i..<i + elements.count {
-            tmp += numbers[j]
-            numsSet.insert(tmp)
+            numsSet.insert(Array(numbers[i...j]).reduce(0, +))
         }
     }
     
