462a332bf576596316d033d394e760228e186fda
김다빈 <84852135+kdb1217@users.noreply.github.com>
2024-08-22 00:59:31 +0900

    [level 2] Title: 전화번호 목록, Time: 95.41 ms, Memory: 30.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -0,0 +1,9 @@
+def solution(phone_book):
+    
+    sorted_phone_book = sorted(phone_book)
+
+    for i in range(len(phone_book) - 1):
+        if sorted_phone_book[i + 1].startswith(sorted_phone_book[i]):
+            return False
+                
+    return True
