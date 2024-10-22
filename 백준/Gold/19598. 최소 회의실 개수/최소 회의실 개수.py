9fd34f90a209a9abd2deecfb845519c9d92d8188
Rash <84852135+kdb1217@users.noreply.github.com>
2024-10-23 02:14:53 +0900

    [Gold V] Title: 최소 회의실 개수, Time: 3108 ms, Memory: 63796 KB -BaekjoonHub

--- 백준/Gold/19598. 최소 회의실 개수/최소 회의실 개수.py
+++ 백준/Gold/19598. 최소 회의실 개수/최소 회의실 개수.py
@@ -1,18 +1,21 @@
-import heapq
-
+from queue import PriorityQueue
 n = int(input())
-arr = [list(map(int, input().split())) for _ in range(n)]
 
-# 회의 시작 시간 순으로 정렬
+arr = list(list(map(int, input().split())) for _ in range(n))
+
+rooms = PriorityQueue()
+
 arr.sort(key=lambda x: (x[0], x[1]))
-
-rooms = []
-heapq.heappush(rooms, arr[0][1])
+rooms.put(arr[0][1])
 
 for i in range(1, n):
-    # 가장 먼저 끝나는 회의를 꺼냄
-    if arr[i][0] >= rooms[0]:
-        heapq.heappop(rooms)  # 현재 회의실을 재사용
-    heapq.heappush(rooms, arr[i][1])
+    if not rooms.empty():
+        tmp = rooms.get()
+        if arr[i][0] >= tmp:
+            rooms.put(arr[i][1])
+        else:
+            rooms.put(arr[i][1])
+            rooms.put(tmp)
 
-print(len(rooms))
+print(rooms.qsize())
+
