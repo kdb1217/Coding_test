97049e2fa69720d1da670f60890dc96f89521833
Rash <84852135+kdb1217@users.noreply.github.com>
2024-11-08 02:01:54 +0900

    [Silver I] Title: 곱셈, Time: 96 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -0,0 +1,19 @@
+a, b, c = tuple(map(int, input().split()))
+
+def solution(a, b):
+    if b == 1:
+        return a % c
+
+    half = solution(a, b // 2) % c
+    half = (half * half) % c
+
+    if b % 2 == 0:
+        return half
+
+    if b % 2 == 1:
+        return (half * a) % c
+
+
+answer = solution(a, b) % c
+print(answer)
+
