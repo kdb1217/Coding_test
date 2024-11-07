46a45e4d29e4d8007a3d55088e296e48a05c36a8
Rash <84852135+kdb1217@users.noreply.github.com>
2024-11-07 16:19:29 +0900

    [Gold IV] Title: 알파벳, Time: 7976 ms, Memory: 185216 KB -BaekjoonHub

---
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -0,0 +1,25 @@
+n, m = tuple(map(int, input().split()))
+
+arr = [list(input()) for _ in range(n)]
+visited = [[False] * m for _ in range(n)]
+answer = 0
+
+
+def backtracking(alphas, i, j, cnt):
+    global answer
+    di = [1, -1, 0, 0]
+    dj = [0, 0, 1, -1]
+    answer = max(cnt, answer)
+
+
+    for k in range(4):
+        ni = i + di[k]
+        nj = j + dj[k]
+        if (0 <= ni < n and 0 <= nj < m) and arr[ni][nj] not in alphas:
+            alphas.add((arr[ni][nj]))
+            backtracking(alphas, ni, nj, cnt + 1)
+            alphas.remove((arr[ni][nj]))
+
+
+backtracking(set(arr[0][0]), 0, 0, 1)
+print(answer)
