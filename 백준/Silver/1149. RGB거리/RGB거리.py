e94f62de22fd7076d62e3eebeb824ef070aebc87
Rash <84852135+kdb1217@users.noreply.github.com>
2024-11-08 17:17:04 +0900

    [Silver I] Title: RGB거리, Time: 112 ms, Memory: 110432 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+for i in range(1, n):
+    for j in range(3):
+        if j == 0:
+            arr[i][j] += min(arr[i - 1][j + 1], arr[i - 1][j + 2])
+        elif j == 1:
+            arr[i][j] += min(arr[i - 1][j - 1], arr[i - 1][j + 1])
+        else:
+            arr[i][j] += min(arr[i - 1][j - 1], arr[i - 1][j - 2])
+
+print(min(arr[-1]))
