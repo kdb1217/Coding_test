bffd2e04190557945915f9c29b67c2e00153ad05
Rash <84852135+kdb1217@users.noreply.github.com>
2024-11-12 22:25:07 +0900

    [level 2] Title: 땅따먹기, Time: 218.31 ms, Memory: 32.4 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/README.md
+++ 프로그래머스/2/12913. 땅따먹기/README.md
@@ -1,6 +1,6 @@
 # [level 2] 땅따먹기 - 12913 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12913#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12913) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 12일 22:24:15
+2024년 11월 12일 22:25:05
 
 ### 문제 설명
 
--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
@@ -1,16 +1,7 @@
 def solution(land):
+    answer = 0
 
-    for i in range(1, len(land)):
-        for j in range(4):
-            if j == 0:
-                land[i][j] += max(land[i - 1][j + 1], land[i - 1][j + 2], land[i - 1][j + 3])
-            elif j == 1:
-                land[i][j] += max(land[i - 1][j + 1], land[i - 1][j + 2], land[i - 1][j - 1])
-            elif j == 2:
-                land[i][j] += max(land[i - 1][j + 1], land[i - 1][j - 2], land[i - 1][j - 1])
-            else:
-                land[i][j] += max(land[i - 1][j - 1], land[i - 1][j - 2], land[i - 1][j - 3])
-                
-    answer = max(land[-1])
+    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    print('Hello Python')
 
     return answer
