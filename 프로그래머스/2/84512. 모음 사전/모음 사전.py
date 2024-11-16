584f917dd544f21bfeaf72ec9b40e3d092e5ce45
Rash <84852135+kdb1217@users.noreply.github.com>
2024-11-16 15:53:58 +0900

    [level 2] Title: 모음 사전, Time: 5.63 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,19 +1,5 @@
-def backtracking(n, lst, arr, word):
-    if n == len(word):
-        if word not in lst:
-            lst.add(word)
-        return
-    
-    for i in range(len(arr)):
-        backtracking(n, lst, arr, word + str(arr[i]))
-
 def solution(word):
     arr = ["A","E","I","O","U"]
-    alphaSet = set()
-    
-    
-    for i in range(1, 6):
-        backtracking(i, alphaSet, arr, "")
-    sortedAlpha = sorted(alphaSet)
-    
-    return (sortedAlpha.index(word) + 1)
+    print("AAAE" > "AAAAU")
+    answer = 0
+    return answer
