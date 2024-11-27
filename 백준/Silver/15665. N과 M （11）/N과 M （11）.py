207ef6e69f1cb7f7171cedf723b160627c02d726
Rash <84852135+kdb1217@users.noreply.github.com>
2024-11-27 17:04:54 +0900

    [Silver II] Title: N과 M (11), Time: 384 ms, Memory: 166628 KB -BaekjoonHub

--- 백준/Silver/15665. N과 M （11）/N과 M （11）.py
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.py
@@ -1,21 +1,22 @@
 n, m = map(int, input().split())
-arr = list(map(int, input().split()))
-result = set()
-visited = [False] * len(arr)
+arr = sorted(map(int, input().split()))  # 정렬을 통해 중복을 그룹화
+result = []  # 최종 결과를 저장할 리스트
 
-
-def backtracking(depth, num, results, numbers):
-    if depth == m:
-        strNum = tuple(num)
-        if strNum not in results:
-            result.add(strNum)
+def backtracking(depth, sequence):
+    if depth == m:  # 길이가 m인 순열 완성
+        result.append(sequence[:])  # 완성된 순열 저장
         return
 
+    prev = -1  # 이전에 사용한 숫자 기록
     for i in range(len(arr)):
-        backtracking(depth + 1, num + [arr[i]], results, numbers)
+        # 중복 처리: 같은 depth에서 동일 숫자는 한 번만 사용
+        if prev == arr[i]:
+            continue
+        prev = arr[i]  # 이전 숫자를 현재 숫자로 갱신
+        sequence.append(arr[i])
+        backtracking(depth + 1, sequence)
+        sequence.pop()  # 백트래킹: 상태 복원
 
-
-backtracking(0, [], result, arr)
-answer = sorted(result)
-for i in answer:
-    print(*i)
+backtracking(0, [])
+for seq in result:
+    print(*seq)
