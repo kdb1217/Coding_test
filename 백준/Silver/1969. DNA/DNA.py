834c99e866741c270902e30651197a252683e60c
Rash <84852135+kdb1217@users.noreply.github.com>
2025-02-23 21:11:30 +0900

    [Silver IV] Title: DNA, Time: 104 ms, Memory: 110736 KB -BaekjoonHub

--- 백준/Silver/1969. DNA/DNA.py
+++ 백준/Silver/1969. DNA/DNA.py
@@ -4,25 +4,21 @@ hammingDistance = 0
 dnaName = ""
 dnaInfo = ["A", "C", "G", "T"]
 
-# 각 열을 확인하면서 가장 많이 등장하는 문자 찾기
 for i in range(m):
     curDna = ""
-    num = n + 1  # 초기값을 n+1로 설정 (최대값으로 시작)
-
-    # 각 문자가 몇 번 등장하는지 체크
+    num = n + 1
     for j in range(4):
         curNum = 0
         for k in range(n):
             if dnaInfo[j] != dnaArr[k][i]:
                 curNum += 1
-        if curNum < num:  # Hamming 거리 최소값을 찾음
+        if curNum < num:
             num = curNum
-            curDna = dnaInfo[j]  # 해당 열에서 가장 적합한 문자 선택
-    
-    # 가장 적합한 문자를 dnaName에 추가
+            curDna = dnaInfo[j]
     dnaName += curDna
-    hammingDistance += num  # Hamming 거리 누적
+    hammingDistance += num
 
-# 결과 출력
+
+
 print(dnaName)
 print(hammingDistance)
