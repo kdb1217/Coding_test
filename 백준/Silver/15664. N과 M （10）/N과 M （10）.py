379bbd597e9d73e6ce3a3eca7361e9e7ae35c8c8
김다빈(Rash) <84852135+kdb1217@users.noreply.github.com>
2025-03-27 20:02:53 +0900

    [Silver II] Title: N과 M (10), Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.py
@@ -0,0 +1,28 @@
+n, m = map(int, input().split())
+arr = sorted(map(int, input().split()))
+visited = [0] * n
+answerSet = set()
+
+
+def backtracking(lst, idx):
+    if len(lst) == m:
+        tupledLst = tuple(lst)
+        if tupledLst not in answerSet:
+            answerSet.add(tupledLst)
+        return
+
+    if len(lst) > m:
+        return
+
+    for i in range(idx, len(arr)):
+        if visited[i] == 0:
+            visited[i] = 1
+            backtracking(lst + [arr[i]], i)
+            visited[i] = 0
+
+
+backtracking([], 0)
+answer = sorted(answerSet)
+
+for i in answer:
+    print(*i)
