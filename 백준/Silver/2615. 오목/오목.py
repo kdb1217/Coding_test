e7f687667414846c6234b0859373438ac5afa018
김다빈(Rash) <84852135+kdb1217@users.noreply.github.com>
2025-05-11 14:22:28 +0900

    [Silver I] Title: 오목, Time: 88 ms, Memory: 109412 KB -BaekjoonHub

--- 백준/Silver/2615. 오목/오목.py
+++ 백준/Silver/2615. 오목/오목.py
@@ -1,46 +1,54 @@
 arr = [list(map(int, input().split())) for _ in range(19)]
 
+
 def checkWin(color, location):
-    # ↗ → ↓ ↘ 방향
     di = [-1, 0, 1, 1]
     dj = [1, 1, 0, 1]
 
-    for k in range(4):
+
+    for k in range(len(di)):
         tmpI = location[0]
         tmpJ = location[1]
         cnt = 1
-
-        # 이전 돌이 같은 색이면 중복 → 무시
         pastI = tmpI - di[k]
         pastJ = tmpJ - dj[k]
+
         if 0 <= pastI < 19 and 0 <= pastJ < 19 and arr[pastI][pastJ] == color:
             continue
 
-        # 4개 더 탐색
-        for _ in range(4):
+        for p in range(4):
             tmpI += di[k]
             tmpJ += dj[k]
-            if 0 <= tmpI < 19 and 0 <= tmpJ < 19 and arr[tmpI][tmpJ] == color:
+
+            if tmpI < 0 or tmpI >= 19 or tmpJ < 0 or tmpJ >= 19:
+                tmpI -= di[k]
+                tmpJ -= dj[k]
+                break
+
+            if arr[tmpI][tmpJ] == color:
                 cnt += 1
             else:
                 break
 
-        # 정확히 5개일 때만 인정
         if cnt == 5:
-            # 6번째 돌이 같은 색이면 무효
             afterI = tmpI + di[k]
             afterJ = tmpJ + dj[k]
-            if 0 <= afterI < 19 and 0 <= afterJ < 19 and arr[afterI][afterJ] == color:
-                continue
 
+            if 0 <= afterI < 19 and 0 <= afterJ < 19 and arr[afterI][afterJ] == color:
+                continue  # 6번째 돌도 같은 색이면 무효 (6목 이상)
+
+            # 정확히 5목일 때만 인정
             print(1 if color == 1 else 2)
             print(location[0] + 1, location[1] + 1)
             exit()
 
-# 전체 바둑판 탐색
-for i in range(19):
-    for j in range(19):
+
+for i in range(len(arr)):
+    for j in range(len(arr[0])):
         if arr[i][j] != 0:
-            checkWin(arr[i][j], (i, j))
+            checkWin(arr[i][j], [i, j])
 
 print(0)
+
+
+
